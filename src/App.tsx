9de4b0bd58f8346d43be7eff168562dc35b28874
Konstantinos Liberopoulos <libekonst@gmail.com>
2021-11-29 19:16:23 +0200

    add: theme provider at the root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 import { RootNavigator } from './RootNavigator';
+import { theme } from './ui-kit/theme';
 
 export function App() {
-  return <RootNavigator />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RootNavigator />
+    </ThemeProvider>
+  );
 }
