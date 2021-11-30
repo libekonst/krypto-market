56703abff1c3dd27f7b4db62540b5649ffdd747f
Konstantinos Liberopoulos <libekonst@gmail.com>
2021-11-30 13:35:05 +0200

    change: ellipsis to constant

--- src/lib/shortenLink.spec.ts
+++ src/lib/shortenLink.spec.ts
@@ -1,12 +1,5 @@
 import { shortenLink } from './shortenLink';
 
-test('shortenLink does not affect strings that are smaller than the max rendered char amount', () => {
-  const inputText = 'https://www.google.com/';
-  const got = shortenLink(inputText);
-
-  expect(got).toBe(inputText);
-});
-
 test('shortenLink truncates large texts', () => {
   const inputText =
     'https://www.google.com/search?q=very+very+very+very+very+very+very+very+very+big+text';
@@ -14,4 +7,11 @@ test('shortenLink truncates large texts', () => {
   const want = 'https://www.google.com/search?q=ver...';
 
   expect(got).toBe(want);
+});
+
+test('shortenLink does not affect strings that are smaller than the max rendered char amount', () => {
+  const inputText = 'https://www.google.com/';
+  const got = shortenLink(inputText);
+
+  expect(got).toBe(inputText);
 });
--- src/lib/shortenLink.tsx
+++ src/lib/shortenLink.tsx
@@ -1,8 +1,9 @@
 const MAX_RENDERED_CHARS = 35;
+const ELLIPSIS = '...';
 
 export const shortenLink = (input: string) => {
   if (input.length > MAX_RENDERED_CHARS)
-    return `${input.slice(0, MAX_RENDERED_CHARS)}...`;
+    return `${input.slice(0, MAX_RENDERED_CHARS)}${ELLIPSIS}`;
 
   return input;
 };
