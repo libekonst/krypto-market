fc6e9ccedd169adb2e182d4411f84e1a96a4756a
Konstantinos Liberopoulos <libekonst@gmail.com>
2021-11-30 16:05:13 +0200

    change: hard coded url params to constants

--- src/pages/coin-details/api/fetchCoinDetails.ts
+++ src/pages/coin-details/api/fetchCoinDetails.ts
@@ -1,12 +1,17 @@
 import { CoinDetailsAPIResponse } from './CoinDetailsAPIResponse';
 import { mapToCoinAnalysis } from './mapToCoinAnalysis';
 
-const baseUrl = 'https://api.coingecko.com/api/v3/coins';
+const BASE_URL = 'https://api.coingecko.com/api/v3/coins';
+const WANTS_LOCALIZATION = 'false';
+const WANTS_TICKERS = 'false';
+const WANTS_SPARKLINE = 'false';
 
 export const fetchCoinDetails = async (id?: string) => {
   if (!id) throw new Error('No coin id');
 
-  const url = `${baseUrl}/${id}?localization=false&tickers=false&sparkline=false`;
+  const url = `${BASE_URL}/${id}?localization=${WANTS_LOCALIZATION}&tickers=${WANTS_TICKERS}&sparkline=${WANTS_SPARKLINE}`;
+  // const res = await fetch(url);
+  // const data: CoinDetailsAPIResponse = await res.json();
 
   await delay(400);
 
