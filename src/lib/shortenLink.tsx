d8b1c3ea5f905842d8fb7165d92edbcbd7840c25
Konstantinos Liberopoulos <libekonst@gmail.com>
2021-11-30 17:34:19 +0200

    change: shortenLink to be easier to follow

--- src/lib/shortenLink.tsx
+++ src/lib/shortenLink.tsx
@@ -2,8 +2,8 @@ const MAX_RENDERED_CHARS = 35;
 const ELLIPSIS = '...';
 
 export const shortenLink = (input: string) => {
-  if (input.length > MAX_RENDERED_CHARS)
-    return `${input.slice(0, MAX_RENDERED_CHARS)}${ELLIPSIS}`;
+  if (input.length <= MAX_RENDERED_CHARS) return input;
 
-  return input;
+  const shortenedText = input.slice(0, MAX_RENDERED_CHARS);
+  return `${shortenedText}${ELLIPSIS}`;
 };
