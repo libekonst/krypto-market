63f95809baff1762488a181bdb8e371ad0bc8f72
Konstantinos Liberopoulos <libekonst@gmail.com>
2021-11-30 02:34:22 +0200

    add: spinner styles

--- src/ui-kit/feedback/Spinner.tsx
+++ src/ui-kit/feedback/Spinner.tsx
@@ -1,3 +1,28 @@
-export function Spinner() {
-  return <p>Loading...</p>;
-}
+import styled, { keyframes } from 'styled-components';
+
+// export function Spinner() {
+//   return <p>Loading...</p>;
+// }
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Spinner = styled.div`
+  display: inline-block;
+  width: 5rem;
+  height: 5rem;
+  background-color: transparent;
+  border-radius: 50%;
+  border: 1px solid;
+  border-top-color: ${props => props.theme.colors.lightShade};
+  border-right-color: ${props => props.theme.colors.darkShade};
+  border-bottom-color: ${props => props.theme.colors.darkShade};
+  border-left-color: ${props => props.theme.colors.darkShade};
+  animation: ${rotate} 1s linear infinite;
+`;
